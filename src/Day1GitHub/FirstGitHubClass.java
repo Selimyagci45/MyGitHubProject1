c99893435b81532e669048fa67206b14c4cac2e6
selim <Selimyagci1512@gmail.com>
2023-01-30 23:11:41 -0500

    I added some changes for math operations.

---
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -0,0 +1,10 @@
+package Day1GitHub;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class FirstGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("Hey GitHub");
+        System.out.println(1+1);
+    }
+}
